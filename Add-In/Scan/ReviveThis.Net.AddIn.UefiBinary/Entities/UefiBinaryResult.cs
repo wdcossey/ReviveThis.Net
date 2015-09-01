5b93cc415e01c1336f12c23df545ad00cb3189ec
William David Cossey <wdcossey@gmail.com>
2015-09-01 22:54:04 +0200

    Busy with WPBT detection.

---
+++ Add-In/Scan/ReviveThis.Net.AddIn.UefiBinary/Entities/UefiBinaryResult.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using ReviveThis.Entities;
+using ReviveThis.Enums;
+using ReviveThis.Interfaces;
+using ReviveThis.Structs;
+
+namespace ReviveThis.AddIn.UefiBinary.Entities
+{
+  public class UefiBinaryResult : IDetectionResultItem, ICustomAddInSection
+  {
+    private readonly ScanResultType _resultType;
+    private readonly string _text;
+    private const string SECTION_ID = "WPBT";
+
+    public UefiBinaryResult(ScanResultType type, string text)
+    {
+      _resultType = type;
+      _text = text;
+    }
+
+    public ScanResultType ResultType
+    {
+      get { return _resultType; }
+    }
+
+    public string LegacyString
+    {
+      get { return string.Format("{0} - {1}.", SECTION_ID, _text);  }
+    }
+
+    public CustomAddInSection CustomAddInSection
+    {
+      get { return new CustomAddInSection(SECTION_ID, "Windows Platform Binary Table"); }
+    }
+
+    public bool CanRepair
+    {
+      get { return false; }
+    }
+
+    public Task<IDetectionRepairResult> Repair()
+    {
+      return null;
+    }
+  }
+}
---
+++ Add-In/Scan/ReviveThis.Net.AddIn.UefiBinary/UefiBinary.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ReviveThis.AddIn.UefiBinary.Entities;
+using ReviveThis.Entities;
+using ReviveThis.Enums;
+using ReviveThis.Interfaces;
+
+namespace ReviveThis.AddIn.UefiBinary
+{
+  #region
+  [Export(typeof(IDetectionAddIn))]
+  public class UefiBinary : IDetectionAddIn
+  {
+    public string Author
+    {
+      get { return @"William David Cossey / Mark van Tilburg"; }
+    }
+
+    private Version _version = null;
+
+    public Version Version
+    {
+      get
+      {
+        if (_version != null)
+          return _version;
+
+        return _version = new Version(1, 0, 0, 0);
+      }
+    }
+
+    public string Name
+    {
+      get { return @"UEFI Binary Loaders"; }
+    }
+
+    private string[] _description = new string[0];
+
+    public string[] Description
+    {
+      get
+      {
+        if (_description.Any())
+          return _description;
+
+        return
+          _description =
+            new[] { "Scans the system event log for UEFI Binary Loaders (Microsoft Windows Platform Binary Table)." };
+      }
+    }
+
+    public async Task<ICollection<IDetectionResultItem>> Scan()
+    {
+      //EventLog.CreateEventSource("Microsoft-Windows-Subsys-SMSS", "MyNewLog");
+      //EventLog myLog = new EventLog("System", ".", "Microsoft-Windows-Subsys-SMSS");
+      //myLog.WriteEntry("A platform binary was successfully executed.");
+
+      var result = new List<IDetectionResultItem>();
+
+      var eventLog = new EventLog("System", ".", "Microsoft-Windows-Subsys-SMSS");
+
+      foreach (EventLogEntry entry in eventLog.Entries.Cast<EventLogEntry>().Where(w => w.Source == "Microsoft-Windows-Subsys-SMSS"))
+      {
+        //Console.WriteLine(entry.Message);
+
+        result.Add(new UefiBinaryResult(ScanResultType.CustomAddIn, entry.Message));
+        
+      }
+
+
+      return result;
+    }
+
+    public ScanResultType ResultType
+    {
+      get { return ScanResultType.CustomAddIn; }
+    }
+
+    public void Dispose()
+    {
+      //Nothing to dispose?
+    }
+  }
+  #endregion
+
+}
